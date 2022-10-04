f7eba024ff015e7722df4f473a726e47a3facbc4
Mohamed El-Haddad <112962278+Haddad308@users.noreply.github.com>
2022-10-04 16:36:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,7 @@ import time
 
 # Information of Setup 
 Path = "C:\\Program Files (x86)\\chromedriver.exe"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"Accept-Encoding": "gzip, deflate, br",
-"Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8,ar;q=0.7"} 
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(Path,options=options)
-
+#         ...... To see full code call +201281982770 whatsapp
 
 ## Data Container
 Data = {'title':[],
@@ -37,34 +30,14 @@ def get_information(URL):
         Data['price'].append(soup.find("span",{"class":"a-price-whole"}).text.split()[0])
         Data['product_image'].append(soup.find("img",{"id":"landingImage"}).attrs['src'])
         Data['specs'].append(soup.find("div",{"id":"poExpander"}).text)
-        Data['about_item'].append(soup.find("div",{"id":"feature-bullets"}).text)
-        Data['tech_info'].append(soup.find("table",{"id":"productDetails_techSpec_section_1"}).text)
-        Data['additional_info'] .append(soup.find("table",{"id":"productDetails_detailBullets_sections1"}).text)
-        print(f"product scrapped Succefully")
-    except :
-        Data['title'].append(np.nan)
-        Data['price'].append(np.nan)
-        Data['product_image'].append(np.nan)
-        Data['specs'].append(np.nan)
-        Data['about_item'].append(np.nan)
-        Data['tech_info'].append(np.nan)
-        Data['additional_info'] .append(np.nan)
-        print("Error while scraping")
-
+#         ...... To see full code call +201281982770 whatsapp 
 
 
 def get_reviews(URL):
     try : 
         driver.get(URL)
         get_review  = driver.find_element(By.XPATH,'//*[@id="reviews-medley-footer"]/div[2]/a')
-        get_review.send_keys(Keys.RETURN)
-        url_ =  driver.current_url
-        page = requests.get(url_, headers=headers)
-        soup = BeautifulSoup(page.content,'lxml')
-        Data['reviews'].append(soup.find("div",{"id":"cm_cr-review_list"}).text.strip())
-    except : 
-        Data['reviews'].append("No reviews Founded")
-
+#         ...... To see full code call +201281982770 whatsapp
 
 
 # Start point 
@@ -88,23 +61,10 @@ Number_of_pages = int(Number_of_products / Number_of_products_per_page)
 counter = 0
 for iter in range(1,Number_of_pages):
     MAIN_URL = f"https://www.amazon.eg/s?i=electronics&rh=n%3A21832907031&fs=true&page={iter}&language=en&qid=1660139874&ref=sr_pg_2"
-    page_products_links = soup.find_all('a',{"class":"a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"})
-    for url_structure in page_products_links:
-        counter += 1 
-        half_url = url_structure.attrs['href']
-        url = f"https://www.amazon.eg/{half_url}"   
-        print(f"{counter}-",end="") 
-        get_information(url)
-        get_reviews(url)
-        time.sleep(1)
+#         ...... To see full code call +201281982770 whatsapp
         
     
-    
-    print(f"Page ({iter}) Scrapped Successfully")
-    # Scraping 3 pages only 
-    test += 1 
-    if (test == 3):
-        break 
+  
 
 
 
